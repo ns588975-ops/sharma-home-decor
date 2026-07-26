b89c2951579757de6d4c51dc20d7e9080b120c65
Sharma Sharma <ns588975@gmail.com>
2026-07-26 14:25:31 +0530

    Implement button click to call functionality
    Add event listener for button click to initiate a call.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Sharma Home Decor Website Loaded");
 
+  const button = document.querySelector("button");
+
+  if (button) {
+    button.addEventListener("click", function () {
+      window.location.href = "tel:7076735147";
+    });
+  }
+});
